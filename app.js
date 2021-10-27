79370917b26d9f9fb0a23a369cfc139af5c654ca
mostrovski <ostrovskiime@gmail.com>
2021-10-27 20:01:00 +0200

    Get image source for each dinosaur

--- app.js
+++ app.js
@@ -96,6 +96,10 @@ class Dinosaur {
         this.fact = data.fact;
     }
 
+    getImageSource() {
+        return `images/${this.species.toLowerCase()}.png`;
+    }
+
     presentFact(comparableObject) {
         const fact = this.getRandomFactProperty();
 
@@ -161,26 +165,10 @@ const dinosaurs = DATA.map(data => new Dinosaur(data));
 
 // Create Human Object
 
-// Use IIFE to get human data from form
-
-
-// Create Dino Compare Method 1
-// NOTE: Weight in JSON file is in lbs, height in inches.
-
-
-// Create Dino Compare Method 2
-// NOTE: Weight in JSON file is in lbs, height in inches.
-
-
-// Create Dino Compare Method 3
-// NOTE: Weight in JSON file is in lbs, height in inches.
-
-
 // Generate Tiles for each Dino in Array
 
 // Add tiles to DOM
 
 // Remove form from screen
-
 
 // On button click, prepare and display infographic
