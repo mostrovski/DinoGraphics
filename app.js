70c4b6184228e4dd6de17564988fb9455063f3fb
mostrovski <ostrovskiime@gmail.com>
2021-10-30 16:11:50 +0200

    Add more comments

--- app.js
+++ app.js
@@ -320,19 +320,45 @@ class Dinosaur extends Creature {
         this.fact = data.fact;
     }
 
+    /**
+     * Get properties that can be used to present a fact about the dinosaur.
+     *
+     * @returns {string[]}
+     */
     scopeFactProperties() {
         return Object.keys(this)
             .filter(key => key !== 'species' && typeof this[key] !== 'function');
     }
 
+    /**
+     * Randomly choose one from the array of "factual" properties.
+     *
+     * @returns {string}
+     */
     getRandomFactProperty() {
         return getRandomValue(this.scopeFactProperties());
     }
 
+    /**
+     * Based on the given string, return the name of the comparison method.
+     * For example, for "weight", the "compareWeight" will be returned.
+     *
+     * @param {string} attribute
+     * @returns {string}
+     */
     composeComparisonMethodName(attribute) {
         return `compare${capitalizeFirstLetter(attribute)}`;
     }
 
+    /**
+     * Comparison method #1: weight.
+     *
+     * Calculate the difference between the given and own weight, and return
+     * a proper message.
+     *
+     * @param {number} weight
+     * @returns {string}
+     */
     compareWeight(weight) {
         let diff = toPrecision(this.weight - weight);
 
@@ -346,6 +372,15 @@ class Dinosaur extends Creature {
         return `Weight: ${Math.abs(diff)} lbs lighter than you!`;
     }
 
+    /**
+     * Comparison method #2: height.
+     *
+     * Calculate the difference between the given and own height, and return
+     * a proper message.
+     *
+     * @param {number} height
+     * @returns {string}
+     */
     compareHeight(height) {
         let diff = toPrecision(inchesToFeet(this.height) - height);
 
@@ -359,6 +394,14 @@ class Dinosaur extends Creature {
         return `Height: is ${Math.abs(diff)} feet shorter than you!`;
     }
 
+    /**
+     * Comparison method #3: diet.
+     *
+     * Check if the given diet equals to own diet, and return a proper message.
+     *
+     * @param {string} height
+     * @returns {string}
+     */
     compareDiet(diet) {
         if (this.diet.toLowerCase() === diet.toLowerCase()) {
             return 'Diet: same as yours!';
@@ -367,6 +410,19 @@ class Dinosaur extends Creature {
         return `Diet: ${this.diet}`;
     }
 
+    /**
+     * Compose a fact about the dinosaur based on the value of a randomly
+     * chosen property.
+     *
+     * The argument must have weight, height, and diet properties set in order
+     * to be comparable if one of them is chosen as a fact to present.
+     *
+     * @param {Object} comparableObject
+     * @param {number} comparableObject.weight
+     * @param {number} comparableObject.height
+     * @param {string} comparableObject.diet
+     * @returns {string}
+     */
     presentFact(comparableObject) {
         if (this.species === 'Pigeon') {
             return `${this.fact}`;
@@ -383,33 +439,42 @@ class Dinosaur extends Creature {
         return `${capitalizeFirstLetter(fact)}: ${this[fact]}`;
     }
 
+    /**
+     * Compose a template string representing the object.
+     *
+     * @param {Object} comparableObject
+     * @see presentFact
+     * @returns {string}
+     */
     renderGraphics(comparableObject) {
         return `
             <div class="grid-item">
                 <h3>${this.species}</h3>
                 <img src="${this.getImageSource()}" alt="${this.species}">
                 <p>${this.presentFact(comparableObject)}</p>
+                ${this.presentFact}
             </div>
         `;
     }
 }
 
+// While the user is looking around, prepare the dinosaurs.
 const dinosaurs = DATA.map(data => new Dinosaur(data));
 
-// DOM manipulations
+// Watch the form - the source of all human in this project.
 const $form = document.querySelector('form');
 
 $form.addEventListener('submit', (event) => {
+    // Prevent sending the form and hide it.
     event.preventDefault();
     $form.classList.add('hidden');
-
+    // Instantiate the human object and render the graphics.
     const human = new Human(processFormData(
         $form,
         ['name', 'feet', 'inches', 'weight', 'diet']
     ));
-
     const dinoGraphics = dinosaurs.map(dinosaur => dinosaur.renderGraphics(human));
     const allGraphics = insertInTheMiddleOf(dinoGraphics, human.renderGraphics());
-
+    // Present results.
     document.querySelector('#grid').innerHTML = allGraphics.join('');
 });
